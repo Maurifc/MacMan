0a159bcb5375fea76aecd31a00bcf5817139e01b
maurifc <mauri_vrb@hotmail.com>
2017-07-03 19:27:22 -0300

    Add ComputadorController n ComputadorControllerTest (empties files)

---
+++ tests/Feature/app/Http/Controllers/Api/ComputadorControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature\app\Http\Controllers\Api;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ComputadorControllerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
